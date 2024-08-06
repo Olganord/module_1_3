112c64adf4677b1ec2a735b94b263ebbea1121b7
Olganord <156664559+Olganord@users.noreply.github.com>
2024-08-06 13:53:05 +0500

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -27,7 +27,7 @@ print('Name: ' + name)
 age = 66
 print( 'Age: ' + str(age))
 age = age + 1
-print('New Age: ' +  str(age+1))
+print('New Age: ' + str(age + 1))
 is_student = True
 print('is Student: ' + str(is_student))
 
